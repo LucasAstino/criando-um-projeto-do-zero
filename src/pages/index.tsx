3a94c264993c905aed2fef011b3a4ff627aaebd5
kleber <kleberrocha2701@gmail.com>
2021-10-18 23:46:29 -0300

    fim desafio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-/* eslint-disable no-use-before-define */
-import React from 'react';
-import Head from 'next/head';
-
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
-import { parseISO, format } from 'date-fns';
+import Prismic from '@prismicio/client';
+
+import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
 import { getPrismicClient } from '../services/prismic';
-import commonStyles from '../styles/common.module.scss';
 
 import styles from './home.module.scss';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -31,61 +32,95 @@ interface HomeProps {
 }
 
 export const Home: React.FC<HomeProps> = ({ postsPagination }) => {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  function hancleMorePosts(): void {
+    fetch(postsPagination.next_page)
+      .then(res => res.json())
+      .then(jsonData => {
+        const newPosts = jsonData.results.map(post => {
+          return {
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+            data: post.data,
+          };
+        });
+        setPosts(oldPosts => [...oldPosts, ...newPosts]);
+        setNextPage(jsonData.next_page);
+      });
+  }
   return (
     <>
       <Head>
-        <title> Home | space traveling</title>
+        <title>space traveling</title>
       </Head>
-      <div className={styles.container}>
-        <main>
-          {postsPagination.results.map(post => {
-            const { first_publication_date, uid } = post;
-            const { author, subtitle, title } = post.data;
+      <main className={styles.postContainer}>
+        <div className={styles.postList}>
+          {posts.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
 
-            return (
-              <div key={uid} className={styles['post-container']}>
-                <h2>
-                  <Link href={`/post/${uid}`}>
-                    <a>{title}</a>
-                  </Link>
-                </h2>
-                <p>{subtitle}</p>
-                <small>
-                  <FiCalendar />
-                  {first_publication_date}
-                </small>
-                <small>
-                  <FiUser />
-                  {author}
-                </small>
-              </div>
-            );
-          })}
-        </main>
-      </div>
+                <div className={styles.postInfo}>
+                  <time>
+                    <FiCalendar />
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </time>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        {nextPage && (
+          <button
+            className={styles.loadMorePostsButton}
+            type="button"
+            onClick={hancleMorePosts}
+          >
+            Carregar mais posts
+          </button>
+        )}
+      </main>
     </>
   );
 };
-export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query('', { pageSize: 50 });
-
-  const result = postsResponse.results.map((p: Post) => {
-    const { uid, first_publication_date } = p;
-
-    const isoDate = parseISO(first_publication_date);
-    const formated_date = format(isoDate, 'dd MMM yyyy');
-
-    const { author, subtitle, title } = p.data;
-
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.author', 'post.subtitle'],
+    }
+  );
+  const posts = postsResponse.results.map(post => {
     return {
-      uid,
-      first_publication_date: formated_date,
-      data: { title, subtitle, author },
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: post.data,
     };
   });
-
-  return { props: { postsPagination: { results: result, next_page: '50' } } };
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsResponse.next_page,
+      },
+    },
+    revalidate: 60 * 30, // 30 minutos
+  };
 };
+
+export default Home;
